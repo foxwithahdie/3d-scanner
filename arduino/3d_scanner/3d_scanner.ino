6014b690c813c1ecc1a85809902187040b8febb7
ram <ramzey.burdette2005@gmail.com>
2025-09-16 08:46:52 -0400

    tested arduino data-holding capabilities and tested sending and receiving data via serial from python

--- arduino/3d_scanner/3d_scanner.ino
+++ arduino/3d_scanner/3d_scanner.ino
@@ -1,9 +1,41 @@
+// const byte button = 8;
+// #define KI 1024
+
+// PinStatus arr[KI];
+// int counter = 0;
 
 
-void setup() {
+// void setup() {
+//     pinMode(button, INPUT);
+//     Serial.begin(115200);
+//     Serial.setTimeout(1);
+// }
 
-}
+// void loop() {
+//     arr[counter] = digitalRead(button);
+//     if (counter < 1025)
+//         counter++;
+//     if (counter == 1024) {
+//         for (int i = 0; i < 1024; i += 8) {
+//             Serial.print(arr[i]);
+//         }
+//         Serial.println();
+//         for (int i = 0; i < 1024; i += 8) {
+//             arr[i] = LOW;
+//         }
 
-void loop() {
+//     }
 
-}
+    
+// }
+
+void setup() { 
+	Serial.begin(115200); 
+	Serial.setTimeout(1); 
+} 
+void loop() { 
+	while (!Serial.available()); 
+	String x = Serial.readString();
+	Serial.print(x); 
+} 
+
